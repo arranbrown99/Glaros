89d6c1b6448117fa127dd84c9b253daad446b8bc
Andrew Partridge <andrewlpartridge@hotmail.com>
2019-11-20 11:24:23 +0000

    update StockRetriever.py

--- StockRetriever.py
+++ StockRetriever.py
@@ -1,5 +1,21 @@
 # import stock_info module from yahoo_fin
 from yahoo_fin import stock_info as si
+import sys
+
+# Amazon = amzn
+# Microsoft = msft
+# Google = goog
+
+# function for retrieving stock price
+def get_stock_price(stock_name):
+
+    # check stock_name is valid stock name
+    try:
+        si.get_live_price(stock_name)
+        return si.get_live_price(stock_name)
+    except ValueError as e:
+        print("Invalid stock name")
 
 
-print(si.get_live_price("amzn"))
+#print(get_stock_price(sys.argv[1]))
+
