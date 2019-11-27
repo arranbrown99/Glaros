7eabd4c25a6a4c604a9b8d6fbd9d1b34a64b4167
Ross Boyd <2309555b@student.gla.ac.uk>
2019-11-27 18:44:35 +0000

    Update gitignore, requirements, develop dns.py
    dns.py is responding 200 for all get and put requests

--- dns.py
+++ dns.py
@@ -1,7 +1,72 @@
 import requests
+import json
+
+api_key = ''
+api_secret = ''
+
+# To do
+# Improve modularity
+# IO for keys
 
 
-def main(domain, service, api_key='', api_secret=''):
+def get(domain):
+    """retrieve domain records
+    """
+    headers = {'Authorization': f'sso-key {api_key}:{api_secret}'}
+    r = requests.get(
+        f'https://api.godaddy.com/v1/domains/{domain}/records', headers=headers)
+    print(r.json())
+
+
+def put(domain):
+    """Update domain records
+    """
+    headers = {'Authorization': f'sso-key {api_key}:{api_secret}'}
+    url = f'https://api.godaddy.com/v1/domains/{domain}/records/NS'
+
+    payload = json.dumps(
+        [
+            {
+                "data": "ns2.digitalocean.com.",
+                "name": "@",
+                "port": 80,
+                "priority": 10,
+                "protocol": "string",
+                "service": "string",
+                "ttl": 600,
+                "weight": 10
+            },
+            {
+                "data": "ns1.digitalocean.com.",
+                "name": "@",
+                "port": 80,
+                "priority": 10,
+                "protocol": "string",
+                "service": "string",
+                "ttl": 600,
+                "weight": 10
+            },
+            {
+                "data": "ns3.digitalocean.com.",
+                "name": "@",
+                "port": 80,
+                "priority": 10,
+                "protocol": "string",
+                "service": "string",
+                "ttl": 600,
+                "weight": 10
+            }
+        ]
+    )
+
+    headers = {'Authorization': f'sso-key {api_key}:{api_secret}',
+               'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+
+    r = requests.put(url, data=payload, headers=headers)
+    print(r)
+
+
+def main(domain, service):
     """Updates domain name servers to point at new cloud service
 
     """
@@ -11,12 +76,8 @@ def main(domain, service, api_key='', api_secret=''):
 
     # curl -X PUT -H"Authorization: sso-key [API_KEY]:[API_SECRET]""https://api.godaddy.com/v1/domains/{domain}/records"
 
-    url = f"https://api.godaddy.com/v1/domains/{domain}/records"
-    _SSO_KEY_TEMPLATE = f'sso-key {api_key}:{api_secret}'
-    payload = "requestjson"
-    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-
-    r = requests.put()
+    get(domain)
+    put(domain)
 
 
-main('glaros.uk', 'AWS')
+main('GLAROS.UK', 'AWS')
