a1915f443250c51051b6b1d54ef893c7a0425878
ArranBrown <2323915b@student.gla.ac.uk>
2019-11-28 14:00:51 +0000

    started working on azure, however hit block with credentials

--- cloud_service_providers/AzureCSP.py
+++ cloud_service_providers/AzureCSP.py
@@ -1,18 +1,52 @@
-from cloud_service_providers.AbstractCSP import AbstractCSP
-from azure.mgmt 
+from AbstractCSP import AbstractCSP
+import os
+import traceback
+
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.compute.models import DiskCreateOption
 
 class AzureCSP(AbstractCSP):
 
+
+
     def __init__(self):
+
         self.stock_name = "msft"
+
         # Here we need to configure all the things necessary to connect
         # to the Azure instance.
+        self.TENANT_ID = '6e725c29-763a-4f50-81f2-2e254f0133c8'
+
+        self.LOCATION = 'eastus'
+        self.GROUP_NAME = 'cs27'
+        self.VM_NAME = 'cs27VM2'
+
 
     def identify(self):
         print("This was called from an AzureCSP instance.")
 
+    def print_vm(self):
+        virtual_machine = compute_client.virtual_machines.get(
+            GROUP_NAME,
+            VM_NAME
+        )
+        print(virtual_machine)
+
+
     def start_vm(self):
-        pass
+        # Start the VM
+        print('\nStart VM')
+        try:
+            async_vm_start = compute_client.virtual_machines.start(
+                GROUP_NAME, VM_NAME)
+            return 0
+        except:
+            print("Failed to start VM")
+            return
+
 
     def stop_vm(self):
         pass
@@ -25,3 +59,9 @@ class AzureCSP(AbstractCSP):
 
     def upload_file(self):
         pass
+
+if __name__ == '__main__':
+    print("here")
+    azure_vm = Azure_CSP()
+    print(azure_vm.GROUP_NAME)
+
