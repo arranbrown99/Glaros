e9232aa22c40ca6ceb82ad9429067dc260d26985
Ross Boyd <2309555b@student.gla.ac.uk>
2019-12-24 23:29:46 +0000

    Custom Defined Errors
    Defining custom errors which this program can raise, for driver to interpret.

--- dns.py
+++ dns.py
@@ -5,7 +5,7 @@ Call change_service
 
 # To do
 # Improve modularity, extendability
-# Custom error exceptions
+# Error handling for configparser
 # Better docstrings
 
 import requests
@@ -16,12 +16,36 @@ import configparser
 __config_file = 'config.ini'
 
 
-def _get(domain):
-    """Retrieve domain records
+class Error(Exception):
+    """Base class for exceptions"""
+    pass
+
+
+class DNSUpdateError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
     """
-    r = requests.get(
-        f'https://api.godaddy.com/v1/domains/{domain}/records/A/@', headers=headers)
-    return(r.json())
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
+
+def _get_ip(url, headers):
+    """Retrieve domain records
+    Returns current IP address
+    """
+
+    try:
+        response = requests.get(url, headers=headers)
+        ip = response.json()[0]['data']
+        response.raise_for_status()
+        return ip
+    except Exception as e:
+        raise Exception(f"{e}")
 
 
 def _update_ip(ip, headers, url):
@@ -32,8 +56,10 @@ def _update_ip(ip, headers, url):
         [{'data': f'{ip}', 'ttl': 600}])
     try:
         response = requests.put(url, data=payload, headers=headers)
+        print(response)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
+        # This shouldn't happen
         raise Exception(f"{e}, {response.json()['fields']}")
     except requests.exceptions.HTTPError as e:
         raise Exception(f"{e}")
@@ -41,6 +67,7 @@ def _update_ip(ip, headers, url):
 
 def change_service(service):
     """Updates domain name servers to point at new cloud service
+    Will throw DNSUpdateError if update failed
 
     """
 
@@ -49,20 +76,26 @@ def change_service(service):
         config.read(__config_file)
     except Exception as e:
         print(f"Could not read {__config_file}")
+        raise(DNSUpdateError)
 
+    # Read in data from config
     dns = config['dns']
     api_key, api_secret = dns['key'], dns['secret']
-    domain = config['DEFAULT']['domain']
+    domain = dns['domain']
     ip = config[f'{service.lower()}']['ip']
 
+    # Construct variables for requests
     headers = {'Authorization': f'sso-key {api_key}:{api_secret}',
                'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     url = f'https://api.godaddy.com/v1/domains/{domain}/records/A/@'
 
     try:
+        current_ip = _get_ip(url, headers)
+        if(current_ip == ip):
+            pass
         _update_ip(ip, headers, url)
     except Exception as e:
-        print(f"{e}")
+        raise(DNSUpdateError(e, ""))
     else:
         print("Changed IP")
 
