95617cd97fbd2094bbafea70391ba4527cbda57d
Ross Boyd <2309555b@student.gla.ac.uk>
2019-12-24 22:56:31 +0000

    requests error handling

--- dns.py
+++ dns.py
@@ -1,48 +1,71 @@
+"""Update the A-record of the DNS server to point to a new vm
+
+Call change_service
+"""
+
+# To do
+# Improve modularity, extendability
+# Custom error exceptions
+# Better docstrings
+
 import requests
 import json
 import configparser
 
-# To do
-# Improve modularity
-# IO for keys
+
+__config_file = 'config.ini'
 
 
-def get(domain):
+def _get(domain):
     """Retrieve domain records
     """
-    headers = {'Authorization': f'sso-key {api_key}:{api_secret}'}
     r = requests.get(
         f'https://api.godaddy.com/v1/domains/{domain}/records/A/@', headers=headers)
-    print(r.json())
+    return(r.json())
 
 
-def put(domain, ip, headers, url):
+def _update_ip(ip, headers, url):
     """Update domain records
     """
 
     payload = json.dumps(
         [{'data': f'{ip}', 'ttl': 600}])
-    headers = {'Authorization': f'sso-key {api_key}:{api_secret}',
-               'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    try:
+        response = requests.put(url, data=payload, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"{e}, {response.json()['fields']}")
+    except requests.exceptions.HTTPError as e:
+        raise Exception(f"{e}")
 
-    r = requests.put(url, data=payload, headers=headers)
-    print(r)
 
-
-def main(domain):
+def change_service(service):
     """Updates domain name servers to point at new cloud service
 
     """
 
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    api_key = config['KEYS']['api_key']
-    api_secret = config['KEYS']['api_secret']
+    try:
+        config = configparser.ConfigParser()
+        config.read(__config_file)
+    except Exception as e:
+        print(f"Could not read {__config_file}")
+
+    dns = config['dns']
+    api_key, api_secret = dns['key'], dns['secret']
+    domain = config['DEFAULT']['domain']
+    ip = config[f'{service.lower()}']['ip']
 
     headers = {'Authorization': f'sso-key {api_key}:{api_secret}',
                'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     url = f'https://api.godaddy.com/v1/domains/{domain}/records/A/@'
-    put(domain, '1.1.1.1', headers, url)
+
+    try:
+        _update_ip(ip, headers, url)
+    except Exception as e:
+        print(f"{e}")
+    else:
+        print("Changed IP")
 
 
-main('GLAROS.UK')
+if __name__ == "__main__":
+    change_service('aws')
