64136f6a9dd7daa129870b4e676308decf51a427
Ross Boyd <2309555b@student.gla.ac.uk>
2019-12-24 20:46:40 +0000

    configparser for api keys
    Closes #60
    
    Added configparser module requirement
    Added config.ini to git ignore
    Added code to support configuration file

--- dns.py
+++ dns.py
@@ -1,8 +1,6 @@
 import requests
 import json
-
-api_key = ''
-api_secret = ''
+import configparser
 
 # To do
 # Improve modularity
@@ -10,55 +8,20 @@ api_secret = ''
 
 
 def get(domain):
-    """retrieve domain records
+    """Retrieve domain records
     """
     headers = {'Authorization': f'sso-key {api_key}:{api_secret}'}
     r = requests.get(
-        f'https://api.godaddy.com/v1/domains/{domain}/records', headers=headers)
+        f'https://api.godaddy.com/v1/domains/{domain}/records/A/@', headers=headers)
     print(r.json())
 
 
-def put(domain):
+def put(domain, ip, headers, url):
     """Update domain records
     """
-    headers = {'Authorization': f'sso-key {api_key}:{api_secret}'}
-    url = f'https://api.godaddy.com/v1/domains/{domain}/records/NS'
 
     payload = json.dumps(
-        [
-            {
-                "data": "ns2.digitalocean.com.",
-                "name": "@",
-                "port": 80,
-                "priority": 10,
-                "protocol": "string",
-                "service": "string",
-                "ttl": 600,
-                "weight": 10
-            },
-            {
-                "data": "ns1.digitalocean.com.",
-                "name": "@",
-                "port": 80,
-                "priority": 10,
-                "protocol": "string",
-                "service": "string",
-                "ttl": 600,
-                "weight": 10
-            },
-            {
-                "data": "ns3.digitalocean.com.",
-                "name": "@",
-                "port": 80,
-                "priority": 10,
-                "protocol": "string",
-                "service": "string",
-                "ttl": 600,
-                "weight": 10
-            }
-        ]
-    )
-
+        [{'data': f'{ip}', 'ttl': 600}])
     headers = {'Authorization': f'sso-key {api_key}:{api_secret}',
                'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
@@ -66,18 +29,20 @@ def put(domain):
     print(r)
 
 
-def main(domain, service):
+def main(domain):
     """Updates domain name servers to point at new cloud service
 
     """
-    nameserver_dict = {'AWS': ['ns1.aws.com', 'ns2.aws.com'],
-                       'GCP': ['ns1.gcp.com', 'ns2.gcp.com'],
-                       'Azure': ['ns1.azure.com', 'ns2.azure.com']}
 
-    # curl -X PUT -H"Authorization: sso-key [API_KEY]:[API_SECRET]""https://api.godaddy.com/v1/domains/{domain}/records"
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    api_key = config['KEYS']['api_key']
+    api_secret = config['KEYS']['api_secret']
 
-    get(domain)
-    put(domain)
+    headers = {'Authorization': f'sso-key {api_key}:{api_secret}',
+               'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    url = f'https://api.godaddy.com/v1/domains/{domain}/records/A/@'
+    put(domain, '1.1.1.1', headers, url)
 
 
-main('GLAROS.UK', 'AWS')
+main('GLAROS.UK')
