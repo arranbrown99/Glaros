c816af13059f61cdc81863130c14607a14fc1da8
Arran Brown <2323915b@student.gla.ac.uk>
2019-12-27 14:25:29 +0000

    changed start and stop vm so they work

--- cloud_service_providers/AzureCSP.py
+++ cloud_service_providers/AzureCSP.py
@@ -1,9 +1,23 @@
 #from AbstractCSP import AbstractCSP
 import os
 import traceback
-#from azure.cli.core import get_default_cli
-import subprocess
-import json
+from subprocess import call,check_output
+
+
+#uses subprocesses to call the azure cli
+
+#install this first on ubuntu
+#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+#on centOS ie amazon use these commands
+#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+#sudo sh -c 'echo -e "[azure-cli] name=Azure CLI baseurl=https://packages.microsoft.com/yumrepos/azure-cli enabled=1 gpgcheck=1 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+#sudo yum install azure-cli
+
+#then must first be logged in on command line use
+#az login
+#or run the login function in this script 
+#this login stays for 3 months
 
 class AzureCSP():
 
@@ -19,64 +33,51 @@ class AzureCSP():
         self.GROUP_NAME = 'cs27'
         self.VM_NAME = 'cs27VM2'
 
- #   def az_cli(self, args_str):
-  #      args = args_str.split()
-   #     cli = get_default_cli()
-    #    cli.invoke(args)
-     #   if cli.result.result:
-      #      return cli.result.result
-       # elif cli.result.error:
-       #     raise cli.result.error
-       # return True
 
     def identify(self):
         print("This was called from an AzureCSP instance.")
 
-    def print_vm(self):
-        # response = self.az_cli("vm list")
-        # print("vm's: %s" % response)
-        # az vm list -d -o table --query "[?name=='vm-name']"
-        process = subprocess.Popen(['az','vm','list','-d','-o','table','--query','"[name=='+ self.VM_NAME +']"'],stdout=subprocess.PIPE)
-        out, err = process.communicate()
-#        d = json.loads(out)
-        print(out)
-        print()
-        print()
+    def get_info(self):
+        command = ["az", "vm", "list", "-d", "-o", "table", "--query", "[?name=='cs27VM2']"]
+        if  self.execute_commands(["az", "vm", "list", "-d", "-o", "table", "--query", "[?name=='cs27VM2']"]) == 0:
+            return check_output(command)
+
+ 
+
+#        return self.execute_commands(["az", "vm", "list", "-d", "-o", "table", "--query", "[?name=='cs27VM2']"])
 
     def is_running(self):
         pass
 
-    def execute_commands(self, commands):
-        pass
 
     def start_vm(self):
         # Start the VM
         print('\nStart VM')
-        process = subprocess.Popen(['az','vm','start','-g',self.GROUP_NAME,'-n',self.VM_NAME],stdout=subprocess.PIPE)
-        out, err = process.communicate()
-        return 0
-
+        return self.execute_commands(["az","vm","start","-g",self.GROUP_NAME,'-n',self.VM_NAME]) 
+    
     def stop_vm(self):
         # Stop the VM
         print('\nStop VM')
-        process = subprocess.Popen(['az','vm','stop','-g',self.GROUP_NAME,'-n',self.VM_NAME],stdout=subprocess.PIPE)
-        print("Done")
-        out, err = process.communicate()
-        return 0
-
+        return self.execute_commands(["az","vm","deallocate","-g",self.GROUP_NAME,'-n',self.VM_NAME]) 
+    
  
 
-    def execute_command(self):
-        pass
+    def execute_commands(self,commands):
+        return call(commands)
 
     def upload_file(self):
         pass
 
+    def login(self):
+        #this logs in for 3 months
+        return self.execute_commands(["az","login"])
+
+    def logout(self):
+        return self.execute_commands(["az","logout"])
+
 
 if __name__ == '__main__':
     azure_vm = AzureCSP()
-    azure_vm.print_vm()
-    azure_vm.stop_vm()
-    azure_vm.print_vm()
-    azure_vm.start_vm()
-    azure_vm.print_vm()
+    azure_vm.get_info()
+  #  azure_vm.stop_vm()
+
