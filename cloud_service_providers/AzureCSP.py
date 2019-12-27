a8b46967e4943c6088853428feb8430ea87f3fc9
ArranBrown <2323915b@student.gla.ac.uk>
2019-12-27 14:57:53 +0000

    pep8 on AzureCSP

--- cloud_service_providers/AzureCSP.py
+++ cloud_service_providers/AzureCSP.py
@@ -1,23 +1,22 @@
-#from AbstractCSP import AbstractCSP
-import os
-import traceback
-from subprocess import call,check_output
+# from AbstractCSP import AbstractCSP
+
+from subprocess import call, check_output
 
 
-#uses subprocesses to call the azure cli
+# uses subprocesses to call the azure cli
 
-#install this first on ubuntu
-#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# install this first on ubuntu
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-#on centOS ie amazon use these commands
-#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-#sudo sh -c 'echo -e "[azure-cli] name=Azure CLI baseurl=https://packages.microsoft.com/yumrepos/azure-cli enabled=1 gpgcheck=1 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
-#sudo yum install azure-cli
+# on centOS ie amazon use these commands
+# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# sudo sh -c 'echo -e "[azure-cli] name=Azure CLI baseurl=https://packages.microsoft.com/yumrepos/azure-cli enabled=1 gpgcheck=1 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+# sudo yum install azure-cli
 
-#then must first be logged in on command line use
-#az login
-#or run the login function in this script 
-#this login stays for 3 months
+# then must first be logged in on command line use
+# az login
+# or run the login function in this script
+# this login stays for 3 months
 
 class AzureCSP():
 
@@ -33,57 +32,50 @@ class AzureCSP():
         self.GROUP_NAME = 'cs27'
         self.VM_NAME = 'cs27VM2'
 
-
     def identify(self):
         print("This was called from an AzureCSP instance.")
 
     def get_info(self):
         command = ["az", "vm", "list", "-d", "-o", "table", "--query", "[?name=='cs27VM2']"]
-        if  self.execute_commands(["az", "vm", "list", "-d", "-o", "table", "--query", "[?name=='cs27VM2']"]) == 0:
+        if self.execute_commands(["az", "vm", "list", "-d", "-o", "table", "--query", "[?name=='cs27VM2']"]) == 0:
             return check_output(command)
 
- 
-
-#        return self.execute_commands(["az", "vm", "list", "-d", "-o", "table", "--query", "[?name=='cs27VM2']"])
-
     def is_running(self):
-        #fails if the VM_NAME is 'VM running' for example but shouldnt be an issue for us
+        # fails if the VM_NAME is 'VM running' for example but shouldnt be an issue for us
         info = self.get_info()
         if "VM running" in info:
             return True
         else:
             return False
 
-
     def start_vm(self):
         # Start the VM
         print('\nStart VM')
-        return self.execute_commands(["az","vm","start","-g",self.GROUP_NAME,'-n',self.VM_NAME]) 
-    
+        return self.execute_commands(["az", "vm", "start", "-g", self.GROUP_NAME, '-n', self.VM_NAME])
+
     def stop_vm(self):
         # Stop the VM
-        #uses deallocate rather than stop as it costs less money
+        # uses deallocate rather than stop as it costs less money
         print('\nStop VM')
-        return self.execute_commands(["az","vm","deallocate","-g",self.GROUP_NAME,'-n',self.VM_NAME]) 
-    
- 
+        return self.execute_commands(["az", "vm", "deallocate", "-g", self.GROUP_NAME, '-n', self.VM_NAME])
 
-    def execute_commands(self,commands):
+    def execute_commands(self, commands):
         return call(commands)
 
     def upload_file(self):
         pass
 
     def login(self):
-        #this logs in for 3 months
-        return self.execute_commands(["az","login"])
+        # this logs in for 3 months
+        return self.execute_commands(["az", "login"])
 
     def logout(self):
-        return self.execute_commands(["az","logout"])
+        return self.execute_commands(["az", "logout"])
+
 
 def main():
     azure_vm = AzureCSP()
-    if azure_vm.is_running() != True:
+    if not azure_vm.is_running():
         azure_vm.start_vm()
 
     azure_vm.stop_vm()
