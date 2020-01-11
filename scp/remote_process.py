326d53a0a804bcd1421c92b4bf576d6c10477d04
Arran Brown <2323915b@student.gla.ac.uk>
2020-01-11 11:49:06 +0000

    added feature to delete remote files issue #58

--- scp/remote_process.py
+++ scp/remote_process.py
@@ -9,13 +9,20 @@ def remote_cmd(ip_address,username,cmd):
     
     try:
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
+        return True
     except:
         print("command failed")
         return 
 
 def remote_python(ip_address,username,python_file):
+    #nohup so process on child vm continues even after logout of parent vm
+    # & to run in the background
     cmd = 'nohup python ' + python_file + ' &'
     return remote_cmd(ip_address,username,cmd)
 
-    
-
+def remote_remove(ip_address,username,remote_filepath):
+    #-r to delete directories
+    #-f to delete automatically without human confirmation
+    #this could be dangerous if we accidently delete important files but everything should be backed up
+    cmd = 'rm -r -f ' + remote_filepath
+    return remote_cmd(ip_address,username,cmd)
--- scp/test_remote_process.py
+++ scp/test_remote_process.py
@@ -4,7 +4,17 @@ ip_address = '34.196.229.24'
 username = 'test2'
 
 def test_remote():
-    remote_process.remote_python(ip_address,username,'infinite.py')
+    assert(remote_process.remote_python(ip_address,username,'infinite.py')) == True,"should run a python program in the background"
+    assert(remote_process.remote_remove(ip_address,username,'test.txt')) == True,"should delete the file test.txt if it exists"
+    assert(remote_process.remote_remove(ip_address,username,'scp')) == True,"should delete the directory scp if it exists"
+    assert(remote_process.remote_remove(ip_address,username,'vm_scp.py')) == True,"should delete the file vm_scp.py if it exists"
+    assert(remote_process.remote_remove(ip_address,username,'nohup.out')) == True,"should delete the file test.txt if it exists"
+    assert(remote_process.remote_remove(ip_address,username,'cs27-main')) == True,"should delete the file test.txt if it exists"
+
+
+
+
+
 
 if __name__ == '__main__':
     test_remote()
