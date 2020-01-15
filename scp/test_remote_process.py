7efb69b2ba74b3d768ffd7f11ff7d5f9b70aa4d3
Anthony <anthony@gawain.lan>
2020-01-15 15:04:10 +0000

    rewrite tests using unittest

--- scp/test_remote_process.py
+++ scp/test_remote_process.py
@@ -1,21 +1,19 @@
+import unittest
 import remote_process
 
 ip_address = "54.194.29.151"
 username = 'ec2-user'
 
-def test_remote():
-#    assert(remote_process.remote_python(ip_address,username,'infinite.py')) == True,"should run a python program in the background"
-    assert(remote_process.remote_remove(ip_address,username,'test.txt')) == True,"should delete the file test.txt if it exists"
-    assert(remote_process.remote_remove(ip_address,username,'scp')) == True,"should delete the directory scp if it exists"
-    assert(remote_process.remote_remove(ip_address,username,'vm_scp.py')) == True,"should delete the file vm_scp.py if it exists"
-    assert(remote_process.remote_remove(ip_address,username,'nohup.out')) == True,"should delete the file test.txt if it exists"
-    assert(remote_process.remote_remove(ip_address,username,'cs27-main')) == True,"should delete the file test.txt if it exists"
+class TestRemoteProcess(unittest.TestCase):
 
-
-
-
+    def test_remote(self):
+        # Delete specified file or directory if it exists
+        self.assertTrue(remote_process.remote_remove(ip_address,username,'test.txt'))
+        self.assertTrue(remote_process.remote_remove(ip_address,username,'scp'))
+        self.assertTrue(remote_process.remote_remove(ip_address,username,'vm_scp.py'))
+        self.assertTrue(remote_process.remote_remove(ip_address,username,'nohup.out'))
+        self.assertTrue(remote_process.remote_remove(ip_address,username,'cs27-main'))
 
 
 if __name__ == '__main__':
-    test_remote()
-    print("All tests passed OK.")
+    unittest.main()
--- testStockRetriever.py
+++ testStockRetriever.py
@@ -1,4 +1,5 @@
 # tests for StockRetriever
+import unittest
 import StockRetriever
 
 # list for good test input
@@ -7,39 +8,35 @@ good_l = ['amzn', 'goog', 'msft', ]
 # list for bad test input
 bad_l = ['foooo']
 
+class TestStockRetrieverMethods(unittest.TestCase):
 
-def test_get_stock_price():
-    """test  a correct and incorrect input for get_stock_price
-    """
-    assert StockRetriever.get_stock_price(good_l[0]) is not None
-    assert StockRetriever.get_stock_price(good_l[1]) is not None
-    assert StockRetriever.get_stock_price(good_l[2]) is not None
+    def test_get_stock_price(self):
+        """test  a correct and incorrect input for get_stock_price
+        """
+        # Test good input
+        self.assertIsNotNone(StockRetriever.get_stock_price(good_l[0]))
+        self.assertIsNotNone(StockRetriever.get_stock_price(good_l[1]))
+        self.assertIsNotNone(StockRetriever.get_stock_price(good_l[2]))
+        # Test bad input
+        self.assertIsNone(StockRetriever.get_stock_price(bad_l[0]))
 
-    assert StockRetriever.get_stock_price(bad_l[0]) is None
+    def test_calculate_difference(self):
+        """test  a correct and incorrect input for calculate_difference
+        """
+        # Test good input
+        self.assertIsNotNone(StockRetriever.calculate_difference(good_l[0]))
+        # Test bad input
+        self.assertIsNone(StockRetriever.calculate_difference(good_l[0]))
 
+    def test_get_stock_data(self):
+        """test that get_stock_data returns a dictionary
+        """
+        self.assertIs(type(StockRetriever.get_stock_data(good_l)), dict)
 
-def test_calculate_difference():
-    """test  a correct and incorrect input for calculate_difference
-    """
-    assert StockRetriever.calculate_difference(good_l[0]) is not None
-    assert StockRetriever.calculate_difference(bad_l[0]) is None
-
-
-def test_get_stock_data():
-    """test that get_stock_data returns a dictioanry
-    """
-    assert type(StockRetriever.get_stock_data(good_l)) == dict
-
-
-def test_best_stock():
-    """test that best_stock returns a string
-    """
-    assert type(StockRetriever.best_stock(good_l)) == str
-
+    def test_best_stock(self):
+        """test that best_stock returns a string
+        """
+        self.assertIs(type(StockRetriever.best_stock(good_l)), str)
 
 if __name__ == '__main__':
-    test_get_stock_price()
-    test_calculate_difference()
-    test_get_stock_data()
-    test_best_stock()
-    print("Tests passed OK.")
+    unittest.main()
