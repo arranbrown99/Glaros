40b95ded0a9a9548ab9c227e26b97ae21f910b6a
Andrew Partridge <andrewlpartridge@hotmail.com>
2020-01-29 13:47:06 +0000

    fixed test_azure imports

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -1,6 +1,8 @@
 import unittest
-import Driver
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from cloud_service_providers.AzureCSP import AzureCSP
+
 
 
 # tests use AzureTesting VM
