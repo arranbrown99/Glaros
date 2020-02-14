a47e7c7945d1e301a91f42bb25f54eb802aae7d8
Ross Boyd <2309555b@student.gla.ac.uk>
2020-02-14 11:16:00 +0000

    Rm . Dockerfile Update Settings, ./Dashboard/Dockerfile

--- Dockerfile
+++
@@ -1,1 +0,0 @@
-ENV PYTHONPATH "${PYTHONPATH}:${pwd}"
--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -133,4 +133,4 @@ STATICFILES_DIRS = [
 ]
 
 # STATIC_URL = "/staticfiles/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
