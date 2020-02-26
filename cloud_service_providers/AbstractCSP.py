5c4c95f4b41cb8f4b1f6a41e180712c5bde92498
Arran Brown <2323915b@student.gla.ac.uk>
2020-02-26 14:53:55 +0000

    Feature/refactor driver

--- cloud_service_providers/AbstractCSP.py
+++ cloud_service_providers/AbstractCSP.py
@@ -2,10 +2,47 @@ from abc import ABC, abstractmethod
 
 
 class AbstractCSP(ABC):
+    class Error(Exception):
+        '''
+        Base class for exceptions
+        '''
+        pass
+
+    class InvalidCSPError(Error):
+        '''
+        Exception raised for errors in the input.
+
+        Attributes
+        ------
+            expression -- input expression in which the error occurred
+            message -- explanation of the error
+        '''
+
+        def __init__(self, expression, message):
+            self.expression = expression
+            self.message = message
+
     stock_name = ""
     username = ""
     ui_colour = "rgb(0,0,0)"
     formal_name = ""
+
+    @classmethod
+    def get_csp(cls, stock_string):
+        types = cls.__subclasses__()
+        for concrete in types:
+            if concrete.stock_name == stock_string:
+                return concrete()
+        raise cls.InvalidCSPError(stock_string, "Invalid Input Parameter ")
+
+    @classmethod
+    def get_stock_names(cls, ):
+        types = cls.__subclasses__()
+        all_stock_names = []
+        for concrete in types:
+            stock_name = concrete.stock_name
+            all_stock_names.append(stock_name)
+        return all_stock_names
 
     @abstractmethod
     def identify(self):
--- cloud_service_providers/AzureCSP.py
+++ cloud_service_providers/AzureCSP.py
@@ -28,10 +28,9 @@ class AzureCSP(AbstractCSP):
     # Static Variables
     ui_colour = 'rgb(54,162,235)'
     formal_name = 'Azure'
+    stock_name = "msft"
 
     def __init__(self):
-
-        self.stock_name = "msft"
 
         # Here we need to configure all the things necessary to connect
         # to the Azure instance.
