abf6dc4d8f22b95cd0ff868f75727e59e3fa99f8
Pieris Kimonos <2319593k@student.gla.ac.uk>
2020-02-26 13:31:03 +0000

    [FEATURE] glaros.uk redirects to /dashboard/ #122
    - Added a url that catches "/" aka. root, and ridirects to /dashboard/

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+import dashboard_app.views as views
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda _: redirect('index')),  # glaros.uk -> glaros.uk/dashboard/
     path('dashboard/', include('dashboard_app.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
