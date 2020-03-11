174c6d7fc71252f34604685ef1de0739b61cbedb
ArranBrown <2323915b@student.gla.ac.uk>
2020-03-11 11:31:11 +0000

    added hours minutes seconds to database closes issue #163, #158

--- dashboard/dashboard_app/models.py
+++ dashboard/dashboard_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class MigrationEntry(models.Model):
     _from = models.CharField(max_length=20)
     _to = models.CharField(max_length=20)
-    _date = models.DateField(auto_now_add=True)
+    _date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self._date.day}-{self._date.month}-{self._date.year}:\tfrom:{self._from}\tto:{self._to}."
+        return f"{self._date.day}-{self._date.month}-{self._date.year},{self._date.hour},{self._date.minute},{self._date.second}:\tfrom:{self._from}\tto:{self._to}."
